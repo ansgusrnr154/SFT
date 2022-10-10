be3b09cdfdbdd60572f89b8e0d6a5100c567c460
문현국 <ansgusrnr126@naver.com>
2022-10-11 00:47:51 +0900

    SFT63프로 완성본

--- app/src/main/java/com/example/sft_p/Fragment/Auth/MypageActivity.kt
+++ app/src/main/java/com/example/sft_p/Fragment/Auth/MypageActivity.kt
@@ -3,10 +3,29 @@ package com.example.sft_p.Fragment.Auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sft_p.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_mypage.*
 
 class MypageActivity : AppCompatActivity() {
+
+    private val db = FirebaseFirestore.getInstance()
+
+    private  lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mypage)
+
+        auth = FirebaseAuth.getInstance()
+
+        val docRef = db.collection("users").document(auth.currentUser?.uid.toString())
+
+        docRef.get().addOnSuccessListener { documentSnapshot ->
+
+            nickname_area.setText(documentSnapshot.get("nickname").toString())
+
+
+        }
     }
 }
